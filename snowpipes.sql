d0e950a2b8cb23a0e5a7d40f26442b28d5b1288e
Chiranthan Ram <chiranthan@gmail.com>
2024-04-18 21:14:56 +0530

    Update snowpipe creation steps

--- snowpipes.sql
+++ snowpipes.sql
@@ -1,6 +1,6 @@
 USE OUR_FIRST_DB.PUBLIC
 
-CREATE TABLE OUR_FIRST_DB.PUBLIC.EMPLOYEES(
+CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.EMPLOYEES(
     ID int,
     first_name varchar,
     last_name varchar,
@@ -23,7 +23,7 @@ SET STORAGE_ALLOWED_LOCATIONS = ("gcs://snowflakeudemygcpbucket/", "gcs://snowfl
 
 //Checking AWS S3 integration object
 DESC INTEGRATION S3_INT
-    
+
 //Creating a stage from S3 "snowpipe" folder which has already been setup in AWS s3 bucket
 CREATE OR REPLACE STAGE MANAGED_DB.EXTERNAL_STAGES.STAGE_CSV_SNOWPIPE
     STORAGE_INTEGRATION = s3_int
@@ -32,5 +32,28 @@ CREATE OR REPLACE STAGE MANAGED_DB.EXTERNAL_STAGES.STAGE_CSV_SNOWPIPE
 
 list @MANAGED_DB.EXTERNAL_STAGES.STAGE_CSV_SNOWPIPE
 
+//Creating a schema specifically for Pipes
+CREATE OR REPLACE SCHEMA MANAGED_DB.PIPES;
 
+//First step to create a pip is to test copy into the target table from the external stage
+COPY INTO OUR_FIRST_DB.PUBLIC.EMPLOYEES
+FROM @MANAGED_DB.EXTERNAL_STAGES.STAGE_CSV_SNOWPIPE
 
+SELECT count(*) FROM OUR_FIRST_DB.PUBLIC.EMPLOYEES
+
+//Creating the pipe
+CREATE OR REPLACE PIPE MANAGED_DB.PIPES.employee_pipe
+AUTO_INGEST = TRUE
+AS
+COPY INTO OUR_FIRST_DB.PUBLIC.EMPLOYEES
+FROM @MANAGED_DB.EXTERNAL_STAGES.STAGE_CSV_SNOWPIPE
+
+DESC PIPE MANAGED_DB.PIPES.employee_pipe
+
+// Error handling with pipes when they fail to load. First we modify the file format in the stage so that the pipe fails to load a regular csv
+CREATE OR REPLACE FILE FORMAT MANAGED_DB.FILE_FORMATS.CSV_FILEFORMAT
+    TYPE = CSV
+    SKIP_HEADER = 1
+    FIELD_DELIMITER = '|'
+    NULL_IF = ('NULL','null')
+    EMPTY_FIELD_AS_NULL = True
